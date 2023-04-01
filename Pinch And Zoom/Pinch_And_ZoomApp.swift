4050d976fe96cfb86c83cfc6fea2e19a5575637c
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-04-01 22:40:46 +0600

    Project created and resource files added

---
+++ Pinch And Zoom/Pinch_And_ZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_And_ZoomApp.swift
+//  Pinch And Zoom
+//
+//  Created by Md Abir Hossain on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_And_ZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
